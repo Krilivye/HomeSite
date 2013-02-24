92ea61653815fe3a14a4c8fbbf894a489bd16491
Krilivye <pierre.bousquie@gmail.com>
2013-02-24 20:31:57 +0100

    Clean indent

--- js/geekClock.js
+++ js/geekClock.js
@@ -1,83 +1,84 @@
 (function( $ ){
 
-    var currentTime = new Date();
+  var currentTime = new Date();
 
-    var methods = {
-	
-	init : function( options){
-	    var settings = $.extend( {
-		'hours'         : currentTime.getHours(),
-		'minutes'         : currentTime.getMinutes(),
-		'seconds'         : currentTime.getSeconds()
-	    }, options);
-	    
-	    this.geekClock('update',settings);
-	},
-	
-	display: function (options){
-	    this.empty();
-	    this.geekClock('hours',options);
-	    this.append($('<span>').html(':'));
-	    this.geekClock('minutes',options);
-	    this.append($('<span>').html(':'));
-	    this.geekClock('seconds',options);
-	},
-	
-	hours : function(options) {
-	    var settings = $.extend( {
-		'hours'         : currentTime.getHours()
-	    }, options);
-	    
+  var methods = {
 
-	    var h = currentTime.getHours();
-	    this.append($('<span>').addClass('geekClock-hours').html(settings.hours));
+    init : function( options){
+      var settings = $.extend( {
+	'hours'         : currentTime.getHours(),
+	'minutes'         : currentTime.getMinutes(),
+	'seconds'         : currentTime.getSeconds()
+      }, options);
 
-	},
-	minutes : function( options) {
-	    var settings = $.extend( {
-		'minutes'         : currentTime.getMinutes()
-	    }, options);
+      this.geekClock('update',settings);
+    },
 
-	    var h = currentTime.getMinutes();
-	    this.append($('<span>').addClass('geekClock-minutes').html(settings.minutes));
+    display: function (options){
+      this.empty();
+      this.geekClock('hours',options);
+      this.append($('<span>').html(':'));
+      this.geekClock('minutes',options);
+      this.append($('<span>').html(':'));
+      this.geekClock('seconds',options);
+    },
 
-	},
-	seconds : function( options) { 
-	    var settings = $.extend( {
-		'seconds'         : currentTime.getSeconds()
-	    }, options);
-      	    
-	    this.append($('<span>').addClass('geekClock-seconds').html(settings.seconds));
+    hours : function(options) {
+      var settings = $.extend( {
+	'hours'         : currentTime.getHours()
+      }, options);
 
-	},
-	update : function(options){
-	    
-	    currentTime=new Date();
-	    var self = this;
-	    setTimeout(
-		function (){
-		    self.geekClock('init');
-		    self.geekClock('display',options);
-		    
-		}
-		,
-		1000
-	    );
-	},
 
-    };
+      var h = currentTime.getHours();
+      this.append($('<span>').addClass('geekClock-hours').html(settings.hours));
 
-    $.fn.geekClock = function( method ) {  
-	
-	if ( methods[method] ) {
-	    methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-	    return this;
-	} else if ( typeof method === 'object' || ! method ) {
-	    return methods.init.apply( this, arguments );
-	} else {
-	    $.error( 'method ' +  method + ' does not exist on jQuery.geekClock' );
-	}  
+    },
+    minutes : function( options) {
+      var settings = $.extend( {
+	'minutes'         : currentTime.getMinutes()
+      }, options);
 
-    };
-    
+      var h = currentTime.getMinutes();
+      this.append($('<span>').addClass('geekClock-minutes').html(settings.minutes));
+
+    },
+    seconds : function( options) {
+      var settings = $.extend( {
+        'seconds'         : currentTime.getSeconds()
+      }, options);
+
+      this.append($('<span>').addClass('geekClock-seconds').html(settings.seconds));
+
+    },
+    update : function(options){
+
+      currentTime=new Date();
+      var self = this;
+      setTimeout(
+	function (){
+	  self.geekClock('init');
+	  self.geekClock('display',options);
+
+	}
+      ,
+	1000
+      );
+    }
+
+  };
+
+  $.fn.geekClock = function( method ) {
+
+    if ( methods[method] ) {
+      methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+      return this;
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'method ' +  method + ' does not exist on jQuery.geekClock' );
+      return console.error("Error");
+    }
+
+  };
+
 })( jQuery );
--- js/test/geekClockTest.js
+++ js/test/geekClockTest.js
@@ -1,67 +1,67 @@
 $(document).ready(function(){
 
-	var testdiv = $('#test');
+  var testdiv = $('#test');
 
-	QUnit.testDone = function (name, failures, total) {
-		testdiv.empty();
-	}
-    
-	module("Geek Clock");
-	test("hour", function() {
-	  var currentTime = new Date();
-      var h = currentTime.getHours();
-	  
-	  testdiv.geekClock('hours',{'hours':h});
+  QUnit.testDone = function (name, failures, total) {
+    testdiv.empty();
+  }
 
-	  equal($('span.geekClock-hours').html(), h, "Hour is not consistent");
-	  
-	});
-	
-	test("minutes", function() {
-	  var currentTime = new Date();
-	  var m = currentTime.getMinutes();
-	  
-	  testdiv.geekClock('minutes');
-	  
-	  equal($('span.geekClock-minutes').html(), m, "Minutes are not consistent");
-	});
-	
-	test("seconde", function() {
-	  var currentTime = new Date();
-	  var s = currentTime.getSeconds();
-	  
-	  testdiv.geekClock('seconds');
-	  
-	  equal($('span.geekClock-seconds').html(), s, "Seconds are not consistent");
-	});
-	
-	
-	test("Display Time", function() {
-	  var currentTime = new Date();
-	  var h = currentTime.getHours();
-	  var s = currentTime.getSeconds();
-	  var m = currentTime.getMinutes();
-	  
-	  testdiv.geekClock('display');
-	  
-	  equals(testdiv.text(), h+":"+m+":"+s, "Global Clock");
-	});
-	
-	test("Update Time", function() {
-	  var currentTime = new Date();
-	  var h = currentTime.getHours();
-	  var s = currentTime.getSeconds();
-	  var m = currentTime.getMinutes();
-	  
-	  testdiv.geekClock();
-	  stop();
-	  setTimeout(
-			function (){
-					start();
-				  equals(testdiv.text(), h+":"+m+":"+s, "Global Clock");
-			}
-			,
-			1000
-		);
-	});
+  module("Geek Clock");
+  test("hour", function() {
+    var currentTime = new Date();
+    var h = currentTime.getHours();
+
+    testdiv.geekClock('hours',{'hours':h});
+
+    equal($('span.geekClock-hours').html(), h, "Hour is not consistent");
+
+  });
+
+  test("minutes", function() {
+    var currentTime = new Date();
+    var m = currentTime.getMinutes();
+
+    testdiv.geekClock('minutes');
+
+    equal($('span.geekClock-minutes').html(), m, "Minutes are not consistent");
+  });
+
+  test("seconde", function() {
+    var currentTime = new Date();
+    var s = currentTime.getSeconds();
+
+    testdiv.geekClock('seconds');
+
+    equal($('span.geekClock-seconds').html(), s, "Seconds are not consistent");
+  });
+
+
+  test("Display Time", function() {
+    var currentTime = new Date();
+    var h = currentTime.getHours();
+    var s = currentTime.getSeconds();
+    var m = currentTime.getMinutes();
+
+    testdiv.geekClock('display');
+
+    equal(testdiv.text(), h+":"+m+":"+s, "Global Clock");
+  });
+
+  test("Update Time", function() {
+    var currentTime = new Date();
+    var h = currentTime.getHours();
+    var s = currentTime.getSeconds();
+    var m = currentTime.getMinutes();
+
+    testdiv.geekClock();
+    stop();
+    setTimeout(
+      function (){
+	start();
+	equal(testdiv.text(), h+":"+m+":"+s, "Global Clock");
+      }
+    ,
+      1000
+    );
+  });
 });
